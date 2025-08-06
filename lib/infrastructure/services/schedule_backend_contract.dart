b220ae22f7e73ee5be527a131524e73e2c80c585
eltonmorais <elton@bellugasolutions.com.br>
2025-08-06 19:20:07 -0300

    :construction: Building schedule mock

---
+++ lib/infrastructure/services/schedule_backend_contract.dart
@@ -0,0 +1,14 @@
+import 'package:unifast_portal/infrastructure/services/dal/dto/schedule/event_dto.dart';
+import 'package:unifast_portal/infrastructure/services/dal/dto/schedule/event_summary_dto.dart';
+import 'package:unifast_portal/infrastructure/services/dal/dto/schedule/event_type_dto.dart';
+
+abstract class ScheduleBackendContract {
+
+  Future<EventSummaryDTO> getScheduleSummary();
+
+  Future<EventDTO> getEvent(String eventId);
+
+  Future<List<EventDTO>> getEventsByDate(DateTime date);
+
+  Future<List<EventDTO>> filterEvents({EventTypeDTO? type, String? courseItemId});
+}
