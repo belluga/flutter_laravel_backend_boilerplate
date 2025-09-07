ed7e6c3623e249d798290bd75bc625da28ea1b5b
Elton Morais <elton@bellugasolutions.com.br>
2025-09-07 10:09:00 -0300

    :package: Upgrade packages

--- lib/domain/app_data/app_data_web.dart
+++ lib/domain/app_data/app_data_web.dart
@@ -1,21 +1,6 @@
-import 'dart:js_interop';
+import 'package:web/web.dart' as web;
 
 import 'package:belluga_boilerplate/domain/app_data/app_type.dart';
-
-@JS()
-@staticInterop
-@anonymous
-class AppDataJS {}
-
-extension AppDataExtension on AppDataJS {
-  external String get hostname;
-  external String get href;
-  external String get port;
-}
-
-@JS()
-@staticInterop
-external AppDataJS get appDataJS;
 
 class AppData {
   final AppType appType = AppType.web;
@@ -25,9 +10,9 @@ class AppData {
   late String device;
 
   Future<void> initialize() async {    
-    port = appDataJS.port;
-    hostname = appDataJS.hostname;
-    href = appDataJS.href;
+    port = web.window.location.port;
+    hostname = web.window.location.hostname;
+    href = web.window.location.href;
     device = "web";
   }
 
